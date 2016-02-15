9e7aa4567ba3098a1574870686a7b14e380f9e18
Andy <aknott68@gmail.com>
2016-02-15 22:25:29 +0000

    First version of Helo world in C Sharp

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world.");
+            Console.ReadKey(); // This forces console output to stay on screen until key hit
+        }
+    }
+}
